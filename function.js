d4bab441ad5a99945f8df49cec078b3e5bb047e5
iashjan <ashjanot@gmail.com>
2019-10-02 02:08:37 +0300

    homeworksolution

---
+++ function.js
@@ -0,0 +1,76 @@
+Functions - Problem Set
+Problems
+currencyConverter(currency, amount)
+Define a function currencyConverter that takes two arguments an amount and a currency (USD, GBP, EGP, BD) and returns the converted version of the amount in Riyals. Use the if-then-else construct available in Javascript.
+
+Hint :
+
+1 Riyal = 4.68 Egyptian pound
+1 Riyal = 0.10 Bahraini dinar
+
+var rates = (4.68, 0.10)
+
+var USD = rates[1];
+var GBP = rates[2];
+var EGP = rates[3];
+var BD = rates[4];
+
+
+
+let currencyConverte = (function())
+let riyal = 0;
+
+function currencyConverte (amount, currency)
+
+    if (currancy == USD){
+    riyal = amount * USD;
+} else if(currancy == GBP){
+    riyal = amount * GBP;
+  }else if (currancy == EGP){
+        riyal = amount / EGP;
+  }eles if (currancy == BD){
+            riyal = amount * BD;
+  }else {
+  alrt("invalid input")
+ }
+           
+       currancy("riyal" * "USD")
+       currancy("riyal" * "GBP")
+       currancy("riyal" * "EGP")
+       currancy("riyal" * "BD")
+       
+   
+
+
+
+
+isCharacterAVowel(character)
+Write a function isCharacterAVowel that takes a character (i.e. a string of length 1) and returns true if it is a vowel and false, otherwise.
+
+
+
+var str = "i, e, a";
+var length = 1;
+function isCharacterAVowel = ("i, e, a"){
+  let str = 1
+}if (true){
+  console.log("vowel")
+}else {
+  console.log("non")
+
+
+
+pow(base, exponent)
+Define a function pow that replicates the Math.pow() method in JavaScript's Math object.
+
+
+
+function pow (): = (3, 7, 4, -7, -2, 0.5) {
+    
+    document.write(Math.pow(7, 3));
+    document.write(Math.pow(4, 0.5));
+    document.write(Math.pow(7, -2));
+    document.write(Math.pow(-7, 0.5));
+}
+pow = (document.write)
+console.log(document.write)
