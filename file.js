7beb966ba3337cd1e376cd08386151cac6d78ee6
“bosheca” <bosheca@protonmail.com>
2019-10-02 08:35:34 +0300

    HW Done

---
+++ file.js
@@ -0,0 +1,36 @@
+
+// Question - 1
+
+function currencyConverter(currency, amount){
+    let rs ;
+    if(currency == "USD"){
+      rs = amount * 3.75;
+    }else  if(currency == "GBP"){
+      rs = amount * 4.60;
+    }else  if(currency == "EGP"){
+      rs = amount * 0.23;
+    }else  if(currency == "BD"){
+      rs = amount * 9.96;
+    }
+    console.log(amount + ' ' + currency + ' equals to ' + rs + ' Riyals')
+  }
+  currencyConverter('USD', 27)
+
+  // Question - 2
+
+  function isCharacterAVowel(c) {
+    var vowels = ['a','e','i','o','u'];
+    if (vowels.indexOf(c) !== -1) {
+      return true;
+    }
+    return false;
+  }
+  isCharacterAVowel()
+
+
+    // Question - 3
+
+    function mathPow() {
+        console.log(Math.pow(7, 3));  
+      }
+      mathPow(); 
