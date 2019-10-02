eec2713e7990a2fd2f8f62321460b4443c2f11fe
Tareq Alotaibi <tareqdev@protonmail.com>
2019-10-02 08:40:51 +0300

    Home work

--- solutions/currencyConverter.js
+++ solutions/currencyConverter.js
@@ -1,8 +1,16 @@
 function currencyConventer(saudiRiyal) {
-    let usd =  3.75
-    console.log(Math.round(saudiRiyal * usd)); 
+    let usd =  3.75;
+    let gbp = 4.61;
+    let egp = 0.23;
+    let bd = 9.95;
+
+    console.log("SAR to USD " + Math.round(saudiRiyal * usd)); 
+    console.log("SAR to BGP " + Math.round(saudiRiyal * gbp)); 
+    console.log("SAR to EGP " + Math.round(saudiRiyal * egp)); 
+    console.log("SAR to BD " + Math.round(saudiRiyal * bd)); 
+
 
 
 }
 
-currencyConventer(5.5);
+currencyConventer(44);
